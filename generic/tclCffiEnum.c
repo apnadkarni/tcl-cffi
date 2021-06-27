3dff412a8a80e18c3c87444bd303d45b42a822cd
Ashok P. Nadkarni <apnmbx-wits@yahoo.com>
2021-06-27 17:18:34 +0530

    RFE #46 enum sequence

--- generic/tclCffiEnum.c
+++ generic/tclCffiEnum.c
@@ -8,6 +8,18 @@
 #include "tclCffiInt.h"
 #include <sys/types.h>
 
+/* Function: CffiEnumFind
+ * Returns the value of an enum member.
+ *
+ * Parameters:
+ * ipCtxP - context in which the enum is defined
+ * enumObj - name of the enum
+ * nameObj - name of the member
+ * valueObjP - location to store the value of the member
+ *
+ * Returns:
+ * *TCL_OK* on success, *TCL_ERROR* on failure.
+ */
 CffiResult
 CffiEnumFind(CffiInterpCtx *ipCtxP,
              Tcl_Obj *enumObj,
@@ -31,6 +43,20 @@ CffiEnumFind(CffiInterpCtx *ipCtxP,
     }
 }
 
+/* Function: CffiEnumBitmask
+ * Calculates a bitmask by doing a bitwise-OR of elements in a list
+ *
+ * Parameters:
+ * ipCtxP - context in which the enum is defined
+ * enumObj - name of the enum
+ * valueListObj - list whose elements are to be OR-ed
+ * maskP - location to store the result of OR-ing the elements
+ *
+ * The elements may be integers or names of members of the enum.
+ *
+ * Returns:
+ * *TCL_OK* on success, else *TCL_ERROR* on failure.
+ */
 CffiResult
 CffiEnumBitmask(CffiInterpCtx *ipCtxP,
                 Tcl_Obj *enumObj,
@@ -63,9 +89,7 @@ CffiEnumBitmask(CffiInterpCtx *ipCtxP,
 }
 
 static CffiResult
-CffiEnumDefineCmd(CffiInterpCtx *ipCtxP,
-                  int objc,
-                  Tcl_Obj *const objv[])
+CffiEnumDefineCmd(CffiInterpCtx *ipCtxP, int objc, Tcl_Obj *const objv[])
 {
     Tcl_HashEntry *heP;
     Tcl_Interp *ip = ipCtxP->interp;
@@ -89,9 +113,45 @@ CffiEnumDefineCmd(CffiInterpCtx *ipCtxP,
 }
 
 static CffiResult
-CffiEnumEntriesCmd(CffiInterpCtx *ipCtxP,
-                   int objc,
-                   Tcl_Obj *const objv[])
+CffiEnumSequenceCmd(CffiInterpCtx *ipCtxP, int objc, Tcl_Obj *const objv[])
+{
+    Tcl_HashEntry *heP;
+    Tcl_Interp *ip = ipCtxP->interp;
+    Tcl_Obj *enumObj;
+    Tcl_Obj **names;
+    int nNames;
+    int start;
+    int newEntry;
+    int i;
+
+    CFFI_ASSERT(objc == 4 || objc == 5);
+
+    CHECK(CffiNameSyntaxCheck(ip, objv[2]));
+    CHECK(Tcl_ListObjGetElements(ip, objv[3], &nNames, &names));
+
+    if (objc == 5)
+        CHECK(Tclh_ObjToInt(ip, objv[4], &start));
+    else
+        start = 0;
+
+    heP = Tcl_CreateHashEntry(&ipCtxP->enums, (char *)objv[2], &newEntry);
+    if (! newEntry)
+        return Tclh_ErrorExists(ip, "Enum", objv[2], NULL);
+
+    /* We will create as list and let it be shimmered to dictionary as needed */
+    enumObj = Tcl_NewListObj(2 * nNames, NULL);
+    for (i = 0; i < nNames; ++i) {
+        Tcl_ListObjAppendElement(NULL, enumObj, names[i]);
+        Tcl_ListObjAppendElement(NULL, enumObj, Tcl_NewIntObj(start++));
+    }
+
+    Tcl_IncrRefCount(enumObj);
+    Tcl_SetHashValue(heP, enumObj);
+    return TCL_OK;
+}
+
+static CffiResult
+CffiEnumMembersCmd(CffiInterpCtx *ipCtxP, int objc, Tcl_Obj *const objv[])
 {
     Tcl_HashEntry *heP;
 
@@ -106,9 +166,7 @@ CffiEnumEntriesCmd(CffiInterpCtx *ipCtxP,
 }
 
 static CffiResult
-CffiEnumListCmd(CffiInterpCtx *ipCtxP,
-                int objc,
-                Tcl_Obj *const objv[])
+CffiEnumListCmd(CffiInterpCtx *ipCtxP, int objc, Tcl_Obj *const objv[])
 {
     Tcl_SetObjResult(ipCtxP->interp,
                      Tclh_ObjHashEnumerateEntries(&ipCtxP->enums,
@@ -156,12 +214,12 @@ CffiEnumObjCmd(ClientData cdata,
     CffiInterpCtx *ipCtxP = (CffiInterpCtx *)cdata;
     int cmdIndex;
     static const Tclh_SubCommand subCommands[] = {
-        {"define", 2, 2, "ENUM ENUMDEFS", CffiEnumDefineCmd},
+        {"define", 2, 2, "ENUM MEMBERS", CffiEnumDefineCmd},
         {"delete", 1, 1, "PATTERN", CffiEnumDeleteCmd},
-        {"entries", 1, 1, "ENUM", CffiEnumEntriesCmd},
+        {"members", 1, 1, "ENUM", CffiEnumMembersCmd},
         {"list", 0, 1, "?PATTERN?", CffiEnumListCmd},
-        {NULL}
-    };
+        {"sequence", 2, 3, "ENUM MEMBERNAMES ?START?", CffiEnumSequenceCmd},
+        {NULL}};
 
     CHECK(Tclh_SubCommandLookup(ip, subCommands, objc, objv, &cmdIndex));
     return subCommands[cmdIndex].cmdFn(ipCtxP, objc, objv);
